b119536cec31aedbd44f8d429563daf26b09a23e
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-27 19:26:36 +0200

    Két szám összegétől számol felfelé 100-ig

---
+++ 040. migazosszegnemszaz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _040.migazosszegnemszaz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot!");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérek még egy számot!");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+
+            int i = szam1 + szam2;
+
+            while (i < 101)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
