956753cb53b61beefeb14e4c8582f5aab2dfc3d9
DGYKKSZKI <124397426+Rattlesnake19720612@users.noreply.github.com>
2023-05-14 22:54:50 +0200

    Update Program.cs

--- 040. migazosszegnemszaz/Program.cs
+++ 040. migazosszegnemszaz/Program.cs
@@ -27,3 +27,13 @@ namespace _040.migazosszegnemszaz
         }
     }
 }
+
+/*Ez a kód egy egyszerű C# programot tartalmaz, amely két számot kér be a felhasználótól, majd ezeket összeadja. Ezután a program egy ciklusban kiírja az összeadás eredményét egészen addig, amíg az eredmény el nem éri vagy meghaladja a 101-et.
+
+A program futását a Main függvény vezérli, amely a program belépési pontja. Először a program kiírja a konzolra a "Kérek egy számot!" szöveget, majd beolvassa a felhasználó által megadott számot (szam1). Ezután ugyanezt teszi a "Kérek még egy számot!" szöveggel és a második számmal (szam2).
+
+Ezután az i változóba beállítjuk szam1 és szam2 összegét. Majd egy while ciklusban addig írjuk ki az i értékét a konzolra, amíg az kisebb, mint 101. Aztán minden iterációban növeljük az i értékét eggyel.
+
+Végül a Console.ReadKey() függvény segítségével várunk egy billentyűlenyomást, mielőtt a program véget érne és kilépne.
+
+Ez a program tehát két számot kér be, összeadja őket, majd kiírja az eredményt, majd folyamatosan kiírja az összeget egészen addig, amíg az el nem éri vagy meghaladja a 101-et.*/
